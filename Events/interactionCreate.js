6c726a7aaffe71160b61d2322ca28381cde69498
JohnoUK79 <johnouk79@gmail.com>
2024-06-11 20:07:17 +0100

    Updated Leaderboard

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -1,6 +1,7 @@
 const time = require('../config/timestamp')
 const sql = require("../config/Database");
 const { EmbedBuilder } = require('discord.js');
+const { top10, top20, top30, top40, top50 } = require('../functions/discordFunctions');
 
 module.exports = {
     name: 'interactionCreate',
@@ -8,7 +9,33 @@ module.exports = {
 		guildIcon = interaction.member.guild.iconURL();
 		guildName = interaction.guild.name
 		console.log(`${setDate} - ${interaction.member.displayName} in #${interaction.channel.name} in ${guildName} triggered the ${interaction.commandName} command or ${interaction.customId} interaction.`);
-
+		//Leaderboard Buttons
+        if (interaction.customId === 'Top10') {
+            try {
+            top10(interaction)
+            } catch (err) {console.log(err)}
+            }
+            if (interaction.customId === 'Top20') {
+            try {
+            top20(interaction)
+            } catch (err) {console.log(err)}
+            }
+            if (interaction.customId === 'Top30') {
+            try {
+            top30(interaction)
+            } catch (err) {console.log(err)}
+            }
+            if (interaction.customId === 'Top40') {
+            try {
+            top40(interaction)
+            } catch (err) {console.log(err)}
+            }
+            if (interaction.customId === 'Top50') {
+            try {
+            top50(interaction)
+            } catch (err) {console.log(err)}
+            }
+    
 
         if (!interaction.isChatInputCommand()) return;
     try {
