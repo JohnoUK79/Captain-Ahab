165750b453c5f9d5709098533d46c4fd631dba74
JohnoUK79 <johnouk79@gmail.com>
2025-01-15 21:57:54 +0000

    Warpath Update

---
+++ functions/jurisdictionsChannelIDs.js
@@ -0,0 +1,24 @@
+const jurisdictionsChannelIDs = [
+    //'915034347030073404',  // OP
+    //'938098999007772753',  // NVX
+    //'940013410207281283',  // PH40
+    //'940700957694656583',  // EFO
+    //'958408697703432274',  // PHEA
+    //'959761836054552636',  // New NVX
+    //'964673391917400104', // CAC 
+    //'995668352192229437', //?
+    '1314362756656533514', //Test Server
+    '1051598655343300618', // New PHEA
+    '1005576978767806575', //PH Family Server
+    '874703585626165288', //WARMACHINE
+    '1013022541561995284', //PH40 Reborn
+    '958408697703432274', //PHAK / EA / AA Server
+    '1026959441708470302', //PHU - UNIVERSITY
+    '1029857915458297956', //SE17 Elite Fighters
+    '1069297333608657086', //WodinsWarriors
+    '1089299706959638619', //Warpath Hooligans 
+    ];
+module.exports = {
+    'jurisdictionsChannelIDs':     jurisdictionsChannelIDs,
+
+}
---
+++ functions/servers.js
@@ -0,0 +1,62 @@
+const SERVER_15 = {
+    'Ankara': ['L', 'M', 'V'],
+    'Atlanta': ['L', 'M', 'V'],
+    'Birminghan': ['V', 'L', 'M'],
+    'Brussels': ['V', 'L', 'M'],
+    'Cairo': ['L', 'M', 'V'],
+    'Copenhagen': ['V', 'L', 'M'],
+    'Detroit': ['L', 'M', 'V'],
+    'Edinburgh': ['V', 'L', 'M'],
+    'Geneva': ['V', 'L', 'M'],
+    'Houston': ['L', 'M', 'V'],
+    'Leningrad': ['M', 'V', 'L'],
+    'Los Angeles': ['M', 'V', 'L'],
+    'Madrid': ['L', 'M', 'V'],
+    'Mexico City': ['V', 'L', 'M'],
+    'Naples': ['L', 'M', 'V'],
+    'Sao Paulo': ['V', 'L', 'M'],
+    'Seattle': ['M', 'V', 'L'],
+    'Stalingrad': ['M', 'V', 'L'],
+    'Vienna': ['V', 'L', 'M'],
+    'Washington': ['L', 'M', 'V']
+  };
+  
+  const SERVER_17 = {
+    'Ankara': ['L', 'M', 'V'],
+    'Atlanta': ['V', 'L', 'M'],
+    'Birminghan': ['L', 'M', 'V'],
+    'Brussels': ['V', 'L', 'M'],
+    'Cairo': ['L', 'M', 'V'],
+    'Copenhagen': ['V', 'L', 'M'],
+    'Detroit': ['M', 'V', 'L'],
+    'Edinburgh': ['L', 'M', 'V'],
+    'Geneva': ['V', 'L', 'M'],
+    'Houston': ['V', 'L', 'M'],
+    'Leningrad': ['M', 'V', 'L'],
+    'Los Angeles': ['L', 'M', 'V'],
+    'Madrid': ['V', 'L', 'M'],
+    'Mexico City': ['M', 'V', 'L'],
+    'Naples': ['V', 'L', 'M'],
+    'Sao Paulo': ['M', 'V', 'L'],
+    'Seattle': ['?', '?', '?'],
+    'Stalingrad': ['M', 'V', 'L'],
+    'Vienna': ['?', '?', '?'],
+    'Washington': ['M', 'V', 'L']
+  };
+  
+  module.exports = {
+    '905071244171440128': { allianceName: 'OP', wpServer: 17, citiesGold: SERVER_17 },
+    '979762566861561906': { allianceName: 'FIRE', wpServer: 17, citiesGold: SERVER_17 },
+    '855784080322985984': { allianceName: 'NVX', wpServer: 15, citiesGold: SERVER_15 },
+    '890675370662182913': { allianceName: 'EFO', wpServer: 23  , citiesGold: SERVER_17 },
+    '957182156071903272': { allianceName: 'PHEA', wpServer: 17, citiesGold: SERVER_17 },
+    '837669934452047873': { allianceName: 'TWPC', wpServer: 15, citiesGold: SERVER_15 },
+    '219007607195566080': { allianceName: 'CAC', wpServer: 15, citiesGold: SERVER_15 },
+    '991717149276110928': { allianceName: 'CAC', wpServer: 17, citiesGold: SERVER_17 },
+    '964496256057630720': { allianceName: 'TEST', wpServer: 17, citiesGold: SERVER_17 },
+    '874016988400332841': { allianceName: 'PHWR', wpServer: 17, citiesGold: SERVER_17 },
+    '1029701280077205524': { allianceName: 'SE17', wpServer: 17, citiesGold: SERVER_17 },
+    '843131973131304990': { allianceName: 'PH-U', wpServer: 17, citiesGold: SERVER_17 },
+    '1050700042983444500': { allianceName: 'New PHEA', wpServer: 17, citiesGold: SERVER_17 },
+    '1314362756656533514': { allianceName: 'Test Server', wpServer: 17, citiesGold: SERVER_17 }
+  };
